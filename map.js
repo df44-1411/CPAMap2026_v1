730ff3398b8fc1653d7881a1dcb5f661931f5330
DF44 <69482529+df44-1411@users.noreply.github.com>
2026-01-11 19:58:41 +0000

    Add JS: Church of Aurora

--- map.js
+++ map.js
@@ -623,6 +623,9 @@ Highcharts.mapChart('container', {
 function(chart) {
   // Loop para adicionar as classes CSS (.WV, .ACP, etc)
   chart.series[0].points.forEach(function(point) {
+    if (point.controller === "Church of Aurora"){
+        point.graphic.addClass('COA');
+    }
     if (point.controller === "Warlords of Kosmos"){
         point.graphic.addClass('WOK');
     }
