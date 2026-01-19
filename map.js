1f4660a48c14a70e9ebfae001f53c649795691d8
DF44 <69482529+df44-1411@users.noreply.github.com>
2026-01-19 20:27:00 +0000

    Add JS: Teutons

--- map.js
+++ map.js
@@ -887,6 +887,9 @@ Highcharts.mapChart('container', {
 function(chart) {
   // Loop para adicionar as classes CSS (.WV, .ACP, etc)
   chart.series[0].points.forEach(function(point) {
+    if (point.controller === "Teutons"){
+        point.graphic.addClass('Teutons');
+    }
     if (point.controller === "Locked Land"){
         point.graphic.addClass('LOCKED');
     }
