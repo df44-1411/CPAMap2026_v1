aed4b947b3ac00863192ea59578cf21b9fd6f394
DF44 <69482529+df44-1411@users.noreply.github.com>
2026-01-21 15:13:02 +0000

    Update map.js

--- map.js
+++ map.js
@@ -749,7 +749,7 @@ Highcharts.mapChart('container', {
         buttonOptions: {
             verticalAlign: 'top',
             align: 'right',
-            x: 15,
+            x: 0,
             y: 40,
             width: 20,  
             height: 20, 
