ddb3f6fdcac739bb37d9778a814ebff16311a39b
DF44 <69482529+df44-1411@users.noreply.github.com>
2026-01-23 22:37:52 +0000

    Add JS: Crimson Sovereign

--- map.js
+++ map.js
@@ -887,6 +887,9 @@ Highcharts.mapChart('container', {
 function(chart) {
   // Loop para adicionar as classes CSS (.WV, .ACP, etc)
   chart.series[0].points.forEach(function(point) {
+    if (point.controller === "Crimson Sovereign"){
+        point.graphic.addClass('CS');
+    }
     
     if (point.controller === "Red Team"){
         point.graphic.addClass('OPWSRT');
