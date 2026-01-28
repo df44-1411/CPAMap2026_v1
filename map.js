5403500dfd2d1b8fe4b0cb12f2267c29aee8ea24
DF44 <69482529+df44-1411@users.noreply.github.com>
2026-01-28 21:32:16 +0000

    Map Update: Deu Branco (Effect: None, Invasion: True)

--- map.js
+++ map.js
@@ -515,7 +515,8 @@ var mapData = [
     "continent": "Tundropa",
     "type": "NORMAL",
     "controller": "Green Team",
-    "underInvasion": false
+    "underInvasion": true,
+    "attackerColor": "#0074ad"
   },
   {
     "id": "id62",
