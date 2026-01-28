38d3572fee30368023ddcd53da141476bf9af4c4
DF44 <69482529+df44-1411@users.noreply.github.com>
2026-01-28 02:45:18 +0000

    Invasion: Fiesta by Green Team

--- map.js
+++ map.js
@@ -458,7 +458,8 @@ var mapData = [
     "continent": "Tundropa",
     "type": "NORMAL",
     "controller": "Freeland",
-    "underInvasion": false
+    "underInvasion": true,
+    "attackerColor": "#02990e"
   },
   {
     "id": "id56",
