3b68c04afadd6c259fbd0e9d41c18c37696e46e1
DF44 <69482529+df44-1411@users.noreply.github.com>
2026-01-29 21:13:12 +0000

    Map Update: Migrator (Effect: None, Invasion: True)

--- map.js
+++ map.js
@@ -568,7 +568,8 @@ var mapData = [
     "continent": "Tundropa",
     "type": "NORMAL",
     "controller": "Red Team",
-    "underInvasion": false
+    "underInvasion": true,
+    "attackerColor": "#202024"
   },
   {
     "id": "id68",
