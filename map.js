80f37f8112ca5f04d639b89b296ba96a7d92275d
DF44 <69482529+df44-1411@users.noreply.github.com>
2026-01-29 21:13:30 +0000

    Map Update: Deep Freeze (Effect: None, Invasion: True)

--- map.js
+++ map.js
@@ -486,8 +486,9 @@ var mapData = [
     "continent": "Tundropa",
     "type": "NORMAL",
     "controller": "Blue Team",
-    "underInvasion": false,
-    "hotspot": true
+    "underInvasion": true,
+    "hotspot": true,
+    "attackerColor": "#02990e"
   },
   {
     "id": "id59",
