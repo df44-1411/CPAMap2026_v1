de411abb799bda05a9f40d7c475cff01973ae655
DF44 <69482529+df44-1411@users.noreply.github.com>
2026-01-30 23:47:40 +0000

    Map Update: Ascent (Effect: None, Invasion: True)

--- map.js
+++ map.js
@@ -661,7 +661,8 @@ var mapData = [
     "continent": "Tundropa",
     "type": "NORMAL",
     "controller": "Blue Team",
-    "underInvasion": false
+    "underInvasion": true,
+    "attackerColor": "#e00007"
   },
   {
     "id": "id78",
