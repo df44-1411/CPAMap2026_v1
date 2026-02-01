0062475d98b59a36fdb45e736e6d527b7c8ae0e9
DF44 <69482529+df44-1411@users.noreply.github.com>
2026-02-01 02:31:43 +0000

    Map Update: Half Pipe (Effect: bomb-active, Invasion: False)

--- map.js
+++ map.js
@@ -679,7 +679,8 @@ var mapData = [
     "continent": "Tundropa",
     "type": "NORMAL",
     "controller": "Red Team",
-    "underInvasion": false
+    "underInvasion": false,
+    "effect": "bomb-active"
   },
   {
     "id": "id80",
