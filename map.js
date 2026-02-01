3015864784cad06a539467425fd0e04246acf239
DF44 <69482529+df44-1411@users.noreply.github.com>
2026-02-01 02:32:10 +0000

    Map Update: Rainbow (Effect: bomb-active, Invasion: False)

--- map.js
+++ map.js
@@ -613,7 +613,8 @@ var mapData = [
     "continent": "Tundropa",
     "type": "NORMAL",
     "controller": "Red Team",
-    "underInvasion": false
+    "underInvasion": false,
+    "effect": "bomb-active"
   },
   {
     "id": "id73",
