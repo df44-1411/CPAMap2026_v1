f9103f8d841ed5a152a5110c849feeaec5ae2686
DF44 <69482529+df44-1411@users.noreply.github.com>
2026-02-03 21:34:42 +0000

    Map Update: Beanie (Effect: None, Invasion: False)

--- map.js
+++ map.js
@@ -717,8 +717,7 @@ var mapData = [
     "continent": "Tundropa",
     "type": "NORMAL",
     "controller": "Freeland",
-    "underInvasion": false,
-    "effect": "nuke-active"
+    "underInvasion": false
   },
   {
     "id": "id85",
