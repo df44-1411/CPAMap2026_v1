12763eae5f42dd37a3749d67cd0434e5714679fd
DF44 <69482529+df44-1411@users.noreply.github.com>
2026-02-04 01:32:40 +0000

    Add JS: Phoenix of Club Penguin

--- map.js
+++ map.js
@@ -924,6 +924,9 @@ Highcharts.mapChart('container', {
 function(chart) {
   // Loop para adicionar as classes CSS (.WV, .ACP, etc)
   chart.series[0].points.forEach(function(point) {
+    if (point.controller === "Phoenix of Club Penguin"){
+        point.graphic.addClass('PCP');
+    }
     if (point.controller === "White Troops"){
         point.graphic.addClass('WTCP');
     }
