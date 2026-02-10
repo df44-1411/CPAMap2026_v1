c1f3765608c8eb3d4efa518da5d679cb50589216
DF44 <69482529+df44-1411@users.noreply.github.com>
2026-02-10 23:51:57 +0000

    Map Update: Parka (Effect: None, Invasion: True)

--- map.js
+++ map.js
@@ -713,7 +713,8 @@ var mapData = [
     "continent": "Tundropa",
     "type": "NORMAL",
     "controller": "Freeland",
-    "underInvasion": false
+    "underInvasion": true,
+    "attackerColor": "#043acf"
   },
   {
     "id": "id82",
