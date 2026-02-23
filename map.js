95770964cb01e78580b67b2bd291bcab0a0b22fd
DF44 <69482529+df44-1411@users.noreply.github.com>
2026-02-23 00:38:51 +0000

    Map Update: Mountain (Effect: None, Invasion: False)

--- map.js
+++ map.js
@@ -111,8 +111,7 @@ var mapData = [
     "continent": "Mammothica",
     "type": "NORMAL",
     "controller": "Freeland",
-    "underInvasion": true,
-    "attackerColor": "#8b0000"
+    "underInvasion": false
   },
   {
     "id": "id13",
