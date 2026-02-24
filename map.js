f8b4a9791952c643c0efd5aece37f15ad13e8a41
DF44 <69482529+df44-1411@users.noreply.github.com>
2026-02-24 13:51:21 +0000

    Map Update: Zero Grau (Effect: None, Invasion: True)

--- map.js
+++ map.js
@@ -102,7 +102,8 @@ var mapData = [
     "continent": "Mammothica",
     "type": "NORMAL",
     "controller": "Freeland",
-    "underInvasion": false
+    "underInvasion": true,
+    "attackerColor": "#8b0000"
   },
   {
     "id": "id12",
