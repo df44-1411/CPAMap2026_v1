92fb1dd2c230ace8529097f1b45afb1ef3d87001
DF44 <69482529+df44-1411@users.noreply.github.com>
2026-03-18 12:17:15 +0000

    Map Update: North Pole (Effect: None, Invasion: True)

--- map.js
+++ map.js
@@ -462,7 +462,8 @@ var mapData = [
     "continent": "Tundropa",
     "type": "NORMAL",
     "controller": "Freeland",
-    "underInvasion": false
+    "underInvasion": true,
+    "attackerColor": "#043acf"
   },
   {
     "id": "id53",
