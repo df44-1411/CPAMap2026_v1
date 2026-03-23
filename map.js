b68e46e38592b05e1a8df57f142160a832113d2e
DF44 <69482529+df44-1411@users.noreply.github.com>
2026-03-23 17:07:29 +0000

    Map Update: Tea (Effect: None, Invasion: False)

--- map.js
+++ map.js
@@ -452,8 +452,7 @@ var mapData = [
     "continent": "Tundropa",
     "type": "NORMAL",
     "controller": "Freeland",
-    "underInvasion": true,
-    "attackerColor": "#8b0000"
+    "underInvasion": false
   },
   {
     "id": "id52",
