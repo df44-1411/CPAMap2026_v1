93e476da2d712a6fd479e7facb4c23d4d22edb4f
DF44 <69482529+df44-1411@users.noreply.github.com>
2026-04-13 21:43:46 +0100

    Map Update: Husky (Effect: None, Invasion: True)

--- map.js
+++ map.js
@@ -192,7 +192,8 @@ var mapData = [
     "continent": "Mammothica",
     "type": "NORMAL",
     "controller": "Templars",
-    "underInvasion": false
+    "underInvasion": true,
+    "attackerColor": "#043acf"
   },
   {
     "id": "id22",
