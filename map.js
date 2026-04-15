1e7da99ba6c4336b35768377cd235963681c8ab6
DF44 <69482529+df44-1411@users.noreply.github.com>
2026-04-15 19:26:21 +0100

    Map Update: Brumby (Effect: None, Invasion: True)

--- map.js
+++ map.js
@@ -257,7 +257,8 @@ var mapData = [
     "continent": "Mammothica",
     "type": "NORMAL",
     "controller": "Templars",
-    "underInvasion": false
+    "underInvasion": true,
+    "attackerColor": "#043acf"
   },
   {
     "id": "id29",
