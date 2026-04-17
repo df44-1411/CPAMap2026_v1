6aed1590ec18726fdef20cc8a87092c3a34d8fd5
DF44 <69482529+df44-1411@users.noreply.github.com>
2026-04-17 01:02:01 +0100

    Map Update: Cloudy (Effect: None, Invasion: True)

--- map.js
+++ map.js
@@ -283,7 +283,8 @@ var mapData = [
     "continent": "Mammothica",
     "type": "NORMAL",
     "controller": "Templars",
-    "underInvasion": false
+    "underInvasion": true,
+    "attackerColor": "#043acf"
   },
   {
     "id": "id32",
