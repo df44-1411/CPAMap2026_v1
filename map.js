68c42489e0d82ff5f0fdcda56edfce1622253faa
DF44 <69482529+df44-1411@users.noreply.github.com>
2026-04-19 00:22:50 +0100

    Map Update: Skates (Effect: None, Invasion: True)

--- map.js
+++ map.js
@@ -352,7 +352,8 @@ var mapData = [
     "continent": "Mammothica",
     "type": "NORMAL",
     "controller": "Templars",
-    "underInvasion": false
+    "underInvasion": true,
+    "attackerColor": "#043acf"
   },
   {
     "id": "id40",
