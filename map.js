63a7c8167a05abf6239c0e65fe9a051e546bf4ab
DF44 <69482529+df44-1411@users.noreply.github.com>
2026-06-07 01:12:29 +0100

    Map Update: Southern Lights (Effect: None, Invasion: False)

--- map.js
+++ map.js
@@ -561,8 +561,7 @@ var mapData = [
     "continent": "Tundropa",
     "type": "NORMAL",
     "controller": "Freeland",
-    "underInvasion": true,
-    "attackerColor": "#8b0000"
+    "underInvasion": false
   },
   {
     "id": "id63",
