2320fa3560cbadb905e88e669b54e6ae7b96b690
DF44 <69482529+df44-1411@users.noreply.github.com>
2026-06-20 15:26:04 +0100

    Map: Belly Slide -> Freeland

--- map.js
+++ map.js
@@ -189,8 +189,7 @@ var mapData = [
     "continent": "Mammothica",
     "type": "NORMAL",
     "controller": "Freeland",
-    "underInvasion": true,
-    "attackerColor": "#080808"
+    "underInvasion": false
   },
   {
     "id": "id21",
