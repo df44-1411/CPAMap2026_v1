096668bd97a185ada1f1c2789892a979d49d0b51
DF44 <69482529+df44-1411@users.noreply.github.com>
2026-08-03 18:58:36 +0100

    Map Update: Sparkle (Effect: None, Invasion: True)

--- map.js
+++ map.js
@@ -590,7 +590,8 @@ var mapData = [
     "continent": "Tundropa",
     "type": "NORMAL",
     "controller": "Freeland",
-    "underInvasion": false
+    "underInvasion": true,
+    "attackerColor": "#FFFFFF"
   },
   {
     "id": "id66",
