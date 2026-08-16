3fc4820a0e9b51428c867252b96464cb826c4661
DF44 <69482529+df44-1411@users.noreply.github.com>
2026-08-17 00:45:30 +0100

    Add PY: Greater Warriors

--- army_code.py
+++ army_code.py
@@ -2,6 +2,7 @@ import operator
 import re
 
 search_terms = {
+    "Greater Warriors": "#FFC72C",
     "Masked Bandits": "#2C2D2D",
     "Restricted Very Violent Disney-Abolished Snuggle Cats": "#aac7fe",
     "Zenith Order": "#5c09e2",
