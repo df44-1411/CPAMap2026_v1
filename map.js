e3d60f671adb78801af35496c338ca841a76c045
DF44 <69482529+df44-1411@users.noreply.github.com>
2026-08-17 02:07:23 +0100

    Add JS: Fire Warriors

--- map.js
+++ map.js
@@ -977,6 +977,9 @@ Highcharts.mapChart('container', {
 function(chart) {
   // Loop para adicionar as classes CSS (.WV, .ACP, etc)
   chart.series[0].points.forEach(function(point) {
+    if (point.controller === "Fire Warriors"){
+        point.graphic.addClass('FW');
+    }
     if (point.controller === "Greater Warriors"){
         point.graphic.addClass('GW');
     }
