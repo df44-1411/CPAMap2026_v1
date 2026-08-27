cf3e6baf3e8cd48d9cfc37fe1bacc2da339dba97
DF44 <69482529+df44-1411@users.noreply.github.com>
2026-08-27 22:41:46 +0100

    Map: Puddle -> Freeland

--- map.js
+++ map.js
@@ -495,8 +495,7 @@ var mapData = [
     "continent": "Tundropa",
     "type": "NORMAL",
     "controller": "Freeland",
-    "underInvasion": true,
-    "attackerColor": "#FFC72C"
+    "underInvasion": false
   },
   {
     "id": "id55",
