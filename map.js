e079ccd869749436c96fbec15f4e8367f9bd2f4e
DF44 <69482529+df44-1411@users.noreply.github.com>
2026-09-06 11:29:00 +0100

    Add JS: Zara Larmy

--- map.js
+++ map.js
@@ -976,6 +976,9 @@ Highcharts.mapChart('container', {
 function(chart) {
   // Loop para adicionar as classes CSS (.WV, .ACP, etc)
   chart.series[0].points.forEach(function(point) {
+    if (point.controller === "Zara Larmy"){
+        point.graphic.addClass('ZL');
+    }
     if (point.controller === "Cult of Tung"){
         point.graphic.addClass('CoT');
     }
