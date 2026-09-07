dd391032ccc2e4ad2f6b82863b28201371b4a54d
DF44 <69482529+df44-1411@users.noreply.github.com>
2026-09-07 23:57:32 +0100

    Map Update: Chinook (Effect: None, Invasion: False)

--- map.js
+++ map.js
@@ -11,8 +11,7 @@ var mapData = [
     "continent": "Mammothica",
     "type": "NORMAL",
     "controller": "Freeland",
-    "underInvasion": true,
-    "attackerColor": "#080808"
+    "underInvasion": false
   },
   {
     "id": "id1",
