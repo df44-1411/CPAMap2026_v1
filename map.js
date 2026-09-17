f812daaed9a9455af2d3cb87f354b1a9230ce313
DF44 <69482529+df44-1411@users.noreply.github.com>
2026-09-17 18:06:44 +0100

    Map Update: Thermal (Effect: None, Invasion: True)

--- map.js
+++ map.js
@@ -48,7 +48,8 @@ var mapData = [
     "continent": "Mammothica",
     "type": "CAPITAL",
     "controller": "Templars",
-    "underInvasion": false
+    "underInvasion": true,
+    "attackerColor": "#080808"
   },
   {
     "id": "id5",
