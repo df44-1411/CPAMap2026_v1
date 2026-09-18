f2495266a0c4484cb4fb12317077668922e8ef6b
DF44 <69482529+df44-1411@users.noreply.github.com>
2026-09-18 19:36:08 +0100

    Map Update: Belly Slide (Effect: None, Invasion: True)

--- map.js
+++ map.js
@@ -192,7 +192,8 @@ var mapData = [
     "continent": "Mammothica",
     "type": "NORMAL",
     "controller": "Freeland",
-    "underInvasion": false
+    "underInvasion": true,
+    "attackerColor": "#B1914B"
   },
   {
     "id": "id21",
