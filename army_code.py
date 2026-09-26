4e249337569b2709f816251afa2851e95f9203d6
DF44 <69482529+df44-1411@users.noreply.github.com>
2026-09-26 13:02:27 +0100

    Delete Army PY: United Republic

--- army_code.py
+++ army_code.py
@@ -5,7 +5,6 @@ search_terms = {
     "Black Ops HQ": "#080808",
     "Boxmen of Boxland": "#B1914B",
     "Zara Larmy": "#ED018C",
-    "United Republic": "#0c0c96",
     "Fire Warriors": "#B22222",
     "Greater Warriors": "#FFC72C",
     "Steam Empire": "#dddddd",
